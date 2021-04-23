62cdd25f3ff26d49aefb945420735db4a1ba18be
Yan-Jobs <yan@yanjobs.me>
2021-04-23 19:17:09 +0200

    Add missing colon in  config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,6 +1,6 @@
 const settings = {
   token : "", //your bot token
-  guild_owner_id : "" //the guild owner id where you want to post your changement.
+  guild_owner_id : "", //the guild owner id where you want to post your changement.
   db: {
 	host: "",
 	db_name: "",
