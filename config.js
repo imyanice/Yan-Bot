a8f02f6a80be47bd7819fcc17e04f0d56e094b5c
Yan-Jobs <yan@yanjobs.me>
2021-04-26 10:57:55 +0200

    Initial Commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const settings = {
+  token: "", //your bot token
+  guild_owner_id: "", //the guild owner id where you want to post your changement.
+  db: {
+    host: "127.0.0.1",
+    db_name: "YanBot",
+    user: "YanJobs",
+    mdp: "*Yanice2909207",
+  },
+};
+
+module.exports = settings;
