8166ad09a7b79457cd19a724f2c8c5f493e91cfa
Yan-Jobs <yan@yanjobs.me>
2021-05-06 19:24:42 +0200

    Add ssl mode connection for new mysql database

--- database/db.js
+++ database/db.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2");
 const settings = require("../config");
+const fs = require("fs");
 
 const connection = new mysql.createConnection({
   user: settings.db.user,
   host: settings.db.host,
   database: settings.db.db_name,
   password: settings.db.mdp,
+  ssl: {
+    ca : fs.readFileSync('/ca-certificate.crt')
+  }
 });
 
 module.exports = connection;
