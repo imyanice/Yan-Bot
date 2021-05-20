34b2368d46460bf944187e7f2bd44e10121d8821
Yan Jobs <yan@yanjobs.me>
2021-05-20 13:23:07 +0000

    :tada: Change all files positions !

--- database/db.js
+++ database/db.js
@@ -7,9 +7,9 @@ const connection = new mysql.createConnection({
   host: settings.db.host,
   database: settings.db.db_name,
   password: settings.db.mdp,
-  ssl: {
-    ca: fs.readFileSync(__dirname + "/ca-certificate.crt"),
-  },
+  ssl: {                                                    // Remove these
+    ca: fs.readFileSync(__dirname + "/ca-certificate.crt"), // three lines if 
+  },                                                        // you don't need ssl.
   port: settings.db.port,
 });
 
