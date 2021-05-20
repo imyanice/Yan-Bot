a04a6fc0422177fc80771baaef06f4e3b87a967b
Yan Jobs <>
2021-05-20 14:14:04 +0000

    :broom: Prettierr time !

--- database/db.js
+++ database/db.js
@@ -7,9 +7,10 @@ const connection = new mysql.createConnection({
   host: settings.db.host,
   database: settings.db.db_name,
   password: settings.db.mdp,
-  ssl: {                                                    // Remove these
-    ca: fs.readFileSync(__dirname + "/ca-certificate.crt"), // three lines if 
-  },                                                        // you don't need ssl.
+  ssl: {
+    // Remove these
+    ca: fs.readFileSync(__dirname + "/ca-certificate.crt"), // three lines if
+  }, // you don't need ssl.
   port: settings.db.port,
 });
 
--- yan.js
+++ yan.js
@@ -1,16 +1,13 @@
 const Yan = require("./base/Yan"),
- { registerEvents, registerCommands } = require("./utils/handler"),
- settings = require("./config");
-
+  { registerEvents, registerCommands } = require("./utils/handler"),
+  settings = require("./config");
 
 const client = new Yan();
-
 
 const init = async () => {
   await registerCommands(client, "../commands");
   await registerEvents(client, "../events");
   client.login(client.config.token);
-}
+};
 
 init();
-
